9fea95bb7ebc67bb473463d05dc680de745a8c1e
kamilmuzyka <kamil.muzyka@gmail.com>
2021-03-01 23:43:05 +0100

    Serve static files

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 import express from 'express';
+import path from 'path';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-const PORT = process.env.PORT ?? 8080;
 
+// Constants
+const PORT = process.env.PORT ?? 8080;
+const __dirname = process.env.PWD;
 const app = express();
+
+// Middleware
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
+// API Routes
 app.get('/', (req, res) => {
 	res.sendStatus(200);
 });
 
+// Static Routes
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+// Initialization
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
