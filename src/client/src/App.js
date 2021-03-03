c3b926c811a851685a28698d1c71a8b3e91f47aa
kamilmuzyka <kamil.muzyka@gmail.com>
2021-03-03 23:03:21 +0100

    Set up styled-components

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Navigation from './components/Navigation';
-import View from './components/View';
+import Navigation from './components/Navigation/index';
+import View from './components/View/index';
+import GlobalStyle from './styles/globalStyle';
 
 function App() {
     return (
         <div className="App">
             <Navigation />
             <View />
+            <GlobalStyle />
         </div>
     );
 }
