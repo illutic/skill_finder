ac41caacf8cbd04630aba929e68372ed3e5f6d56
UP933287 <up933287@myport.ac.uk>
2021-03-04 10:19:04 +0000

    Scripts update

--- src/docker/winScript.bat
+++ src/docker/winScript.bat
@@ -2,5 +2,12 @@
 set /p USERNAME="Enter Github Username: "
 set /p token=<TOKEN.txt
 docker login https://docker.pkg.github.com -u %USERNAME% -p %token%
-docker-compose -f skill-finder-docker-compose.yml up
-timeout 5
+docker-compose -f skill-finder-docker-compose.yml up -d
+@echo =================================================================
+@echo 				Commands:
+@echo "npm run client" - Start the development server for the client code
+@echo "npm run server" - Start the API server
+@echo "npm run dev" - Run both servers
+@echo "npm run build" - Build client code
+@echo =================================================================
+docker exec -it docker_skill-finder_1 /bin/sh
