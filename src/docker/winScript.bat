848f2e7bd1cbdea85b04d2e4aac9a35dca1a78ba
UP933287 <up933287@myport.ac.uk>
2021-03-04 16:48:22 +0000

    Database auto initialisation docker

--- src/docker/winScript.bat
+++ src/docker/winScript.bat
@@ -1,26 +1,23 @@
 @echo Make sure you have your TOKEN.txt file and your .env file in the root folder of the project!
 @echo off
-timeout 5
+timeout 2
 cd ..
-IF EXIST TOKEN.txt (
-	IF EXIST .env (
-        COPY .env docker\.env
-		set /p USERNAME="Enter Github Username: "
-		set /p token=<TOKEN.txt
-		docker login https://docker.pkg.github.com -u %USERNAME% -p %token%
-		docker-compose -f docker/skill-finder-docker-compose.yml up -d
-		@echo =================================================================
-		@echo 				Commands:
-		@echo "npm run client" - Start the development server for the client code
-		@echo "npm run server" - Start the API server
-		@echo "npm run dev" - Run both servers
-		@echo "npm run build" - Build client code
-		@echo =================================================================
-		docker exec -it docker_skill-finder_1 /bin/sh
-	) ELSE (
-	  @echo .env File not found!
-	)
+IF EXIST .env (
+    COPY .env docker\.env
+    set /p USERNAME="Enter Github Username: "
+    set /p token=<TOKEN.txt
+    docker login https://docker.pkg.github.com -u %USERNAME% -p %token%
+    docker-compose -f docker/skill-finder-docker-compose.yml up -d
+    @echo =================================================================
+    @echo 				Commands:
+    @echo "npm run client" - Start the development server for the client code
+    @echo "npm run server" - Start the API server
+    @echo "npm run dev" - Run both servers
+    @echo "npm run build" - Build client code
+    @echo =================================================================
+    docker exec -it docker_skill-finder_1 /bin/sh
 ) ELSE (
-@echo TOKEN file not found!
+    @echo .env File not found!
+)
 timeout 5
 )
