a9e8d6e1b4b3fa9d3ecfa3514e34ffd160586b9a
kamilmuzyka <kamil.muzyka@gmail.com>
2021-03-04 18:58:17 +0100

    Transform the signup HOC into a utility function

--- src/client/src/containers/Signup/index.js
+++ src/client/src/containers/Signup/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Form from '../../components/Form/index';
 import FORMS from '../../constants/forms';
-import withSignupHandler from '../../hoc/withSignupHandler';
+import extractFormData from '../../utils/extractFormData';
 
-const Signup = ({ handler }) => {
-    return <Form type={FORMS.signup} handler={handler}></Form>;
+const Signup = () => {
+    const signupHandler = (e) => {
+        e.preventDefault();
+        const formData = extractFormData(e.target);
+        console.log(formData);
+    };
+
+    return <Form type={FORMS.signup} submitHandler={signupHandler}></Form>;
 };
 
-export default withSignupHandler(Signup);
+export default Signup;
