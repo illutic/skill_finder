2183a93aeeb8ebd83f2a0d6115bff5441a562bf5
UP933287 <up933287@myport.ac.uk>
2021-03-05 15:42:24 +0000

    script-touch-up

--- src/docker/winScript.bat
+++ src/docker/winScript.bat
@@ -5,15 +5,14 @@ cd ..
 IF EXIST .env (
     COPY .env docker\.env
     docker system prune
-    docker build -t skill-finder .
-    docker-compose -f docker/skill-finder-docker-compose.yml up -d
+    docker-compose -f docker/docker-compose.yml up -d
     @echo =================================================================
     @echo 				Commands:
     @echo The API server is running Automatically.
     @echo "npm run client" - Start the development server for the client code
     @echo "npm run build" - Build client code
     @echo =================================================================
-    docker exec -it docker_app_1 /bin/sh
+    docker exec -it docker.pkg.github.com/m30819-2020/cw-code-t33/skill-finder:latest /bin/sh
 ) ELSE (
     @echo .env File not found!
 )
