8e778a148f23feb90cb791b57f13cbdcee1cd47b
UP933287 <up933287@myport.ac.uk>
2021-03-06 23:53:13 +0000

    docker scripts

--- src/docker/DeployedDocker/winScript.bat
+++ src/docker/DeployedDocker/winScript.bat
@@ -1,16 +1,18 @@
 @echo off
 cd ..
+cd ..
 IF EXIST .env (
-    COPY .env docker\.env
-    docker-compose -f docker/docker-compose.yml up -d
+    COPY .env docker\DeployedDocker\.env
+    cd docker\DeployedDocker
+    docker-compose up -d
     @echo =================================================================
     @echo 				Commands:
     @echo The API server is running Automatically.
     @echo "npm run client" - Start the development server for the client code
     @echo "npm run build" - Build client code
     @echo =================================================================
-    docker start docker_app_1
-    docker exec -it docker_app_1 /bin/sh
+    docker start deployeddocker_app_1
+    docker exec -it deployeddocker_app_1 /bin/sh
 ) ELSE (
     @echo .env File not found!
 )
@@ -20,15 +22,15 @@ SET /P input="Do you want to stop all the containers? [y]Yes or [n]No or [c]Clea
    goto sub_%input%  
 :sub_y
     @echo STOPPING CONTAINERS!
-    docker stop docker_app_1
-    docker stop docker_db_1
-    docker stop docker_adminer_1
+    docker stop deployeddocker_app_1
+    docker stop deployeddocker_db_1
+    docker stop deployeddocker_adminer_1
     goto:eof
 :sub_c
     @echo STOPPING CONTAINERS!
-    docker stop docker_app_1
-    docker stop docker_db_1
-    docker stop docker_adminer_1
+    docker stop deployeddocker_app_1
+    docker stop deployeddocker_db_1
+    docker stop deployeddocker_adminer_1
     @echo CLEARING CONTAINERS!
     docker system prune -af
 :sub_n
