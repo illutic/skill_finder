8ab15f65bff1fcb211c9557465ef7661df387985
kamilmuzyka <kamil.muzyka@gmail.com>
2021-03-16 20:05:32 +0100

    Extract search query from a URL

--- src/client/src/components/SearchResults/index.js
+++ src/client/src/components/SearchResults/index.js
@@ -1,12 +1,24 @@
+import { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
 import Container from '../Container/index';
 import Subheading from '../Subheading/index';
 
-const SearchResults = () => {
+const SearchResults = ({ location }) => {
+    const [query, setQuery] = useState();
+
+    useEffect(() => {
+        const queryString = location.search.substring(1);
+        const searchParams = new URLSearchParams(queryString);
+        const decodedQuery = searchParams.get('query');
+        setQuery(decodedQuery);
+        // <<< API call >>>
+    }, [location.search]);
+
     return (
         <Container>
-            <Subheading underlined>Results for</Subheading>
+            <Subheading underlined>Results for {query}</Subheading>
         </Container>
     );
 };
 
-export default SearchResults;
+export default withRouter(SearchResults);
