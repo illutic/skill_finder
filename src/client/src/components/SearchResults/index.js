8406455c8006742536fb4c63b3c1746081839d9b
kamilmuzyka <kamil.muzyka@gmail.com>
2021-03-16 20:44:28 +0100

    Create styles for SearchResults component

--- src/client/src/components/SearchResults/index.js
+++ src/client/src/components/SearchResults/index.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import * as Styled from './styled';
 import Container from '../Container/index';
 import Heading from '../Heading/index';
+import ProfilePhoto from '../ProfilePhoto/index';
+import Button from '../Button/index';
 
 const SearchResults = ({ location }) => {
     const [query, setQuery] = useState();
@@ -17,10 +19,53 @@ const SearchResults = ({ location }) => {
 
     return (
         <Container>
-            <Heading>Results for {query}</Heading>
-            <Styled.Results>
-                <Styled.Result></Styled.Result>
-            </Styled.Results>
+            <Styled.Wrapper>
+                <Heading>Results for {query}</Heading>
+                <Styled.Entries>
+                    <Styled.Entry>
+                        <Link to="/profile/1">
+                            <Styled.User>
+                                <ProfilePhoto
+                                    src="https://picsum.photos/100/100"
+                                    size={75}
+                                />
+                                <Styled.Details>
+                                    <Styled.Name>John Smith</Styled.Name>
+                                    <Styled.Title>
+                                        Teachnig Mathematics
+                                    </Styled.Title>
+                                </Styled.Details>
+                            </Styled.User>
+                        </Link>
+                        <Styled.Buttons>
+                            <Button outlined>View profile</Button>
+                            <Button>Reach out</Button>
+                        </Styled.Buttons>
+                    </Styled.Entry>
+                    <Styled.Entry>
+                        <Link to="/profile/1">
+                            <Styled.User>
+                                <ProfilePhoto
+                                    src="https://picsum.photos/100/100"
+                                    size={75}
+                                />
+                                <Styled.Details>
+                                    <Styled.Name>Ian Jones</Styled.Name>
+                                    <Styled.Title>
+                                        Mathematics Passionate
+                                    </Styled.Title>
+                                </Styled.Details>
+                            </Styled.User>
+                        </Link>
+                        <Styled.Buttons>
+                            <Link to="/profile/1">
+                                <Button outlined>View profile</Button>
+                            </Link>
+                            <Button>Reach out</Button>
+                        </Styled.Buttons>
+                    </Styled.Entry>
+                </Styled.Entries>
+            </Styled.Wrapper>
         </Container>
     );
 };
--- src/client/src/components/SearchResults/styled.js
+++ src/client/src/components/SearchResults/styled.js
@@ -1,5 +1,48 @@
 import styled from 'styled-components';
+import SCREENS from '../../constants/screens';
 
-export const Results = styled.section``;
+export const Wrapper = styled.div`
+    margin-bottom: 75px;
+`;
 
-export const Result = styled.div``;
+export const Entries = styled.div`
+    margin-top: 20px;
+`;
+
+export const Entry = styled.div`
+    padding: 15px 0;
+    &:not(:last-child) {
+        border-bottom: 1px solid ${({ theme }) => theme.colors.subtle};
+    }
+    & > a {
+        display: inline-block;
+    }
+    @media (min-width: ${SCREENS.medium}) {
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+export const User = styled.section`
+    display: flex;
+    align-items: center;
+`;
+
+export const Details = styled.div`
+    margin-left: 15px;
+`;
+
+export const Name = styled.h3`
+    font-size: ${({ theme }) => theme.typography.medium};
+    font-family: ${({ theme }) => theme.typography.secondary};
+    line-height: 1.25;
+`;
+
+export const Title = styled.p``;
+
+export const Buttons = styled.div`
+    margin-top: 15px; // to 0
+    & > *:first-child {
+        margin-right: 15px;
+    }
+`;
