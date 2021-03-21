4a82c152d1f8c8a58947bb7b5ff6a06d71ec4788
kamilmuzyka <kamil.muzyka@gmail.com>
2021-03-21 20:05:27 +0100

    Update ContactsDrawer component

--- src/client/src/components/ContactsDrawer/index.js
+++ src/client/src/components/ContactsDrawer/index.js
@@ -4,23 +4,34 @@ import stopPropagation from '../../utils/stopPropagation';
 import ProfileThumbnail from '../ProfileThumbnail/index';
 import ROUTES from '../../constants/routes';
 import { ChatsContext } from '../../contexts/ChatsContextProvider';
+import defaultProfilePhoto from '../../assets/profile.gif';
 
 const ContactsDrawer = ({ isActive }) => {
     const { chats } = useContext(ChatsContext);
 
     return (
         <Styled.ContactsDrawer active={isActive} onClick={stopPropagation}>
-            {chats?.map((chat) =>
-                chat.Users?.map((user) => (
-                    <Styled.Contact key={user.id}>
-                        <ProfileThumbnail
-                            name={`${user.firstName} ${user.lastName}`}
-                            title={user.title}
-                            limited
-                        />
-                    </Styled.Contact>
-                ))
-            )}
+            {chats?.length
+                ? chats.map((chat) =>
+                      chat.Users?.map((user) => (
+                          <Styled.Contact
+                              key={chat.id}
+                              to={`${ROUTES.messages}/${chat.id}`}
+                          >
+                              <ProfileThumbnail
+                                  name={`${user.firstName} ${user.lastName}`}
+                                  title={user.title}
+                                  photo={
+                                      user.profilePhoto
+                                          ? user.profilePhoto
+                                          : defaultProfilePhoto
+                                  }
+                                  limited
+                              />
+                          </Styled.Contact>
+                      ))
+                  )
+                : null}
             {/* Template */}
             {/* <Styled.Contact key={chatId} to={`${ROUTES.messages}/${chatId}`}>
                 <ProfileThumbnail
