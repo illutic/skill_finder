f811ccf6a128169eba3647019cd415767d6fb92d
UP933287 <up933287@myport.ac.uk>
2021-03-23 21:31:29 +0000

    revert to previous chat component

--- src/client/src/components/Chat/index.js
+++ src/client/src/components/Chat/index.js
@@ -1,18 +1,18 @@
-import { useContext, useEffect, useRef } from 'react';
+import { useContext, useEffect, useCallback, useRef, useState } from 'react';
 import { UserContext } from '../../contexts/UserContextProvider';
+import { SocketContext } from '../../contexts/SocketContextProvider';
+import { initialize, leaveChat, joinChat } from '../../helpers/socket';
+import ENDPOINTS from '../../constants/endpoints';
 import * as Styled from './styled';
 import useLocationId from '../../hooks/useLocationId';
-import { useChatSocket } from '../../hooks/useChatSocket.js';
 
 const Chat = ({ toggleContactsDrawer, toggleFilesDrawer }) => {
     const { user } = useContext(UserContext);
+    const [socket] = useContext(SocketContext);
     const { locationId: chatId } = useLocationId();
-    const { socket, messages, setMessages } = useChatSocket(chatId);
+    const [messages, setMessages] = useState([]);
     const messagesContainerRef = useRef();
-    const scrollDown = () => {
-        const messagesContainer = messagesContainerRef.current;
-        messagesContainer.scrollTop = messagesContainer.scrollHeight;
-    };
+    let prevChatId = chatId;
 
     const sendMessage = (e) => {
         e.preventDefault();
@@ -24,14 +24,41 @@ const Chat = ({ toggleContactsDrawer, toggleFilesDrawer }) => {
         }
     };
 
+    const scrollDown = () => {
+        const messagesContainer = messagesContainerRef.current;
+        messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    };
+
+    const loadMessages = useCallback(async () => {
+        if (chatId) {
+            const response = await fetch(`${ENDPOINTS.api}/${chatId}/messages`);
+            const data = await response.json();
+            setMessages(data);
+        }
+    }, [chatId]);
+
+    const leaveCurrentChat = useCallback((chatId) => {
+        leaveChat(socket, prevChatId);
+        setMessages([]);
+        prevChatId = chatId;
+    }, []);
+
+    useEffect(() => {
+        leaveCurrentChat(chatId);
+        initialize(socket);
+        joinChat(socket, chatId);
+        loadMessages();
+    }, [chatId]);
+
     useEffect(() => {
         if (socket) {
             socket.on('message', (message) => {
                 setMessages([...messages, message]);
             });
-            scrollDown();
         }
+        scrollDown();
     }, [messages, socket]);
+
     return (
         <Styled.Chat>
             <Styled.Controls>
