12cce1ffd939f1e3c2e90218fe1cf2c1534a2634
kamilmuzyka <kamil.muzyka@gmail.com>
2021-03-24 22:50:01 +0100

    Add comments to useRequest hook

--- src/client/src/hooks/useRequest.js
+++ src/client/src/hooks/useRequest.js
@@ -11,8 +11,14 @@ export const useRequest = () => {
     const sendRequest = useCallback(
         (toId) => {
             socket.emit('request', toId);
+
+            // Delete requestNotification
+            // and emit X that will make the server
+            // create a new notification for toId.
             socket.emit('requestNotification', toId);
+
             syncRequests();
+            // ^ We will sync notifications here eventually.
         },
         [socket, syncRequests]
     );
@@ -20,20 +26,32 @@ export const useRequest = () => {
     const acceptRequest = useCallback(
         (requestId) => {
             socket.emit('acceptRequest', requestId);
+            // Emit X that will make the server
+            // create a new notification for fromId
+
             syncRequests();
+            // ^ We will sync notifications here eventually.
         },
         [socket, syncRequests]
     );
     const denyRequest = useCallback(
         (requestId) => {
             socket.emit('denyRequest', requestId);
+            // We are not informing users about denied requests
+            // nor do we delete them. Only delete a notification
+            // about the request for toId.
+
             syncRequests();
+            // ^ We will sync notifications here eventually.
         },
         [socket, syncRequests]
     );
 
     useEffect(() => {
         socket.on('acceptedRequest', (acceptedRequest) => {
+            // This socket should create a chatroom for
+            // toId and fromId. Then we synchronise chats for both users here.
+            // I think it's all done already - commenting for clarity.
             syncChats();
         });
     }, [socket, syncRequests, syncChats]);
