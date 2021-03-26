762494bb81dae09f3b30b763d40d96ebb069684c
kamilmuzyka <kamil.muzyka@gmail.com>
2021-03-26 20:58:12 +0100

    Create a controller returning sorted user's requests

--- src/controllers/request-controller.js
+++ src/controllers/request-controller.js
@@ -1,15 +1,38 @@
+import sequelize from 'sequelize';
 import Request from '../models/Request.js';
+
+const { Op } = sequelize;
 
 /** Get User Requests */
 const getRequests = async (req, res) => {
     try {
         const { userId } = req;
-        const requests = await Request.findAll({
+        const acceptedRequests = await Request.findAll({
             where: {
-                toId: userId,
+                [Op.or]: [{ toId: userId }, { fromId: userId }],
+                accepted: true,
+                pending: false,
             },
         });
-        res.status(200).json(requests);
+        const incomingRequests = await Request.findAll({
+            where: {
+                toId: userId,
+                accepted: false,
+                pending: true,
+            },
+        });
+        const sentRequests = await Request.findAll({
+            where: {
+                fromId: userId,
+                accepted: false,
+                pending: true,
+            },
+        });
+        res.status(200).json({
+            acceptedRequests,
+            incomingRequests,
+            sentRequests,
+        });
     } catch (err) {
         res.status(400).json({ error: err.message });
     }
