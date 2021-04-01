bcbe432c085559d6e5a3d63d40324b0790607ec7
George Sigalas <up933287@myport.ac.uk>
2021-04-01 19:38:05 +0100

    API Sign In

--- src/test/UserSignUp.test.js
+++ src/test/UserSignUp.test.js
@@ -1,15 +1,16 @@
 /* eslint-disable no-undef */
 import faker from 'faker';
-import chai from 'chai';
+import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import { signUp } from '../controllers/auth-controller.js';
 import app from '../app.js';
 
-chai.use(chaiHttp);
 const should = chai.should();
-process.env.DB_HOST = 'localhost:5430';
 
-describe('User Sign Up Test', () => {
+describe('User Authentication Test Suite', () => {
+    process.env.DB_HOST = 'localhost:5430';
+    chai.use(chaiHttp);
+    let cookie;
     const userPass = faker.internet.password();
     const mockUser = {
         firstName: faker.name.firstName(),
@@ -18,15 +19,48 @@ describe('User Sign Up Test', () => {
         password: userPass,
         confirmPassword: userPass,
     };
-    it('should create a User', () => {
+    it('Creates a User', () => {
         chai.request(app)
-            .post('/api/auth/signup')
-            .type('form')
+            .post('/auth/signup')
+            .set('content-type', 'application/json')
             .send(mockUser)
             .end((err, res) => {
+                console.log(res.body);
                 res.should.have.status(200);
-                done();
+                expect(res).to.have.cookie('origin');
+                cookie = res.header['set-cookie'];
+                // done();
                 // validateAuthForm(mockUser, FORM_TYPES.signup);
             });
+    });
+    it('Logs In a User', () => {
+        if (cookie) {
+            chai.request(app)
+                .post('/auth/login')
+                .set('cookie', cookie[0])
+                .send({
+                    email: mockUser.email,
+                    password: mockUser.password,
+                })
+                .end((err, res) => {
+                    console.log(res.body);
+                    res.should.have.status(200);
+                    // done();
+                    // validateAuthForm(mockUser, FORM_TYPES.signup);
+                });
+        } else {
+            chai.request(app)
+                .post('/auth/login')
+                .send({
+                    email: mockUser.email,
+                    password: mockUser.password,
+                })
+                .end((err, res) => {
+                    console.log(res.body);
+                    res.should.have.status(200);
+                    // done();
+                    // validateAuthForm(mockUser, FORM_TYPES.signup);
+                });
+        }
     });
 });
