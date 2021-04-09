23a44371a4840669da74c0fec235a615f9a90865
UP933287 <up933287@myport.ac.uk>
2021-04-09 07:30:12 +0100

    websocket authentication test

--- src/test/WebSocket.test.js
+++ src/test/WebSocket.test.js
@@ -1,11 +1,20 @@
 /* eslint-disable no-undef */
 import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import { step } from 'mocha-steps';
+import faker from 'faker';
+import supertest from 'supertest';
 import io from 'socket.io-client';
 import { Server } from 'socket.io';
 import WebSockets from '../sockets/WebSockets.js';
 import httpServer from '../server.js';
+import app from '../app.js';
 
-// const should = chai.should();
+const should = chai.should();
+chai.use(chaiHttp);
+const request = supertest(app);
+let cookie;
+let wsClient;
 describe('Websocket httpServer tests', () => {
     before((done) => {
         const ioServer = new Server(httpServer);
@@ -14,20 +23,68 @@ describe('Websocket httpServer tests', () => {
     });
 
     describe('WebSocket Client Mock', () => {
-        const wsClient = io('http://localhost:8081');
-        wsClient.on('connection', () => console.log('Client connected'));
-        it('PING Test', (done) => {
+        const userPass = faker.internet.password(10, true);
+        const mockUser = {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            password: userPass,
+            confirmPassword: userPass,
+        };
+        step('Create a User', async () => {
+            await request
+                .post('/auth/signup')
+                .set('content-type', 'application/json')
+                .send(mockUser)
+                .then((res) => {
+                    console.log(res.body);
+                    res.should.have.status(200);
+                    expect(res).to.have.cookie('origin');
+                    cookie = res.header['set-cookie'];
+                });
+        });
+        step('Log In a User', async () => {
+            const loginObj = {
+                email: mockUser.email,
+                password: mockUser.password,
+            };
+            await request
+                .post('/auth/login')
+                .set('Content-Type', 'application/json')
+                .send(loginObj)
+                .then((res) => {
+                    console.log(res.body);
+                    res.should.have.status(200);
+                    cookie = res.header['set-cookie'];
+                });
+        });
+        step('Connect Socket', () => {
+            const options = {
+                transportOptions: {
+                    polling: {
+                        extraHeaders: {
+                            Cookie: cookie,
+                        },
+                    },
+                },
+            };
+            wsClient = io('http://localhost:8081', options);
+        });
+        step('PING Test', (done) => {
             wsClient.emit('PING', '');
             wsClient.on('PONG', (data) => {
-                if (data === 'PONG') {
-                    done();
-                }
+                expect(data).to.be.eq('PONG');
+                done();
             });
         }).timeout(5000);
-        it('Authentication Test', (done) => {
+        step('Authentication Test', (done) => {
             wsClient.emit('authentication');
-            done();
+            wsClient.on('authorized', () => {
+                done();
+            });
+            wsClient.on('unauthorized', () => {
+                return false;
+            });
         });
-        // Get Cookie and send it with socket
     });
 });
